51b874703883631651f02db1d73844cfec5d2058
Awslearn12345 <komal.awslearn@gmail.com>
2025-11-12 22:40:22 +0530

    Rename class and update greeting message

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-class Simple{  
+class DevopsKomalclass{  
     public static void main(String args[]){  
      System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
       System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
